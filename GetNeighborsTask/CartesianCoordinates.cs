20b00a873b39784e8d11f643d7c339ad360b335d
Anzhelika Kravchuk <AnzhelikaKravchuk@users.noreply.github.com>
2020-04-26 13:47:01 +0300

    add-task-and-test-projects

---
+++ GetNeighborsTask/CartesianCoordinates.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GetNeighborsTask
+{
+    public static class CartesianCoordinates
+    {
+        public static Point[] GetNeighbors(Point point, int range, params Point[] points)
+        {
+            throw new NotImplementedException("You need to implement this function.");
+        }
+    }
+}
