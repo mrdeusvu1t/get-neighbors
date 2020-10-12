cf8dba0a9ecdddec828d625349f1b0da6feb4b01
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-10-12 06:42:20 +0000

    Upload New File

---
+++ GetNeighbors/CartesianCoordinates.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GetNeighbors
+{
+    public static class CartesianCoordinates
+    {
+        /// <summary>
+        /// Gets from a set of points only points that are h-neighbors for a point with integer coordinates x and y.
+        /// </summary>
+        /// <param name="point">Given point with integer coordinates x and y.</param>
+        /// <param name="h">Distance around a given point.</param>
+        /// <param name="points">A given set of points.</param>
+        /// <returns>Only points that are h-neighbors for a point with integer coordinates x and y.</returns>
+        /// <exception cref="ArgumentNullException">Throw when array points is null.</exception>
+        /// <exception cref="ArgumentException">Throw when h-distance is more or equals zero.</exception>
+        public static Point[] GetNeighbors(Point point, int h, params Point[] points)
+        {
+            throw new NotImplementedException("You need to implement this method.");
+        }
+    }
+}
